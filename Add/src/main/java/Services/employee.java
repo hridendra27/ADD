950fea6713032b0d6136cb4bc548e66920a11244
hridendra27 <hridendra27@gmail.com>
2018-03-17 15:44:22 +0530

    firstcommit

---
+++ Add/src/main/java/Services/employee.java
@@ -0,0 +1,31 @@
+package Services;
+
+public class employee {
+private String name;
+private int id;
+
+/*public employee(int id, String name) {
+	this.name = name;
+	this.id = id;
+	}*/
+public employee () {
+	
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public int getId() {
+	return id;
+}
+
+}
